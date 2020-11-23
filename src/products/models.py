12c1500d38eec025b86af67011681947133fa8a0
Prajwal Mani <prajwalmani@users.noreply.github.com>
2020-11-23 15:53:57 +0530

    modelmanagers

--- src/products/models.py
+++ src/products/models.py
@@ -1,11 +1,29 @@
 from django.db import models
+import os 
+import random
 
+def get_filename_ext(filename):
+    base_name=os.path.basename(filename)
+    name,ext=os.path.splitext(base_name)
+    return name,ext
+def upload_image_path(instance,filename):
+    new_filename=random.randint(1,45878995)
+    name,ext=get_filename_ext(filename)
+    final_filename='{new_filename}{ext}'.format(new_filename=new_filename,ext=ext)
+    return "products/{new_filename}/{final_filename}".format(new_filename=new_filename,final_filename=final_filename)
 # Create your models here.
+class ProductManager(models.Manager):
+    def get_by_id(self,id):
+        qs= self.get_queryset().filter(id=id)
+        if qs.count()==1:
+            return qs.first()
+        return None
+
 class Product(models.Model):
     title=models.CharField(max_length=120)
     description=models.TextField()
     price=models.DecimalField(decimal_places=2,max_digits=20,default=39.99) 
-    image=models.FileFiled(upload_to='products/')
-
+    image=models.ImageField(upload_to=upload_image_path,null=True,blank=True)
+    objects=ProductManager()
     def __str__(self):
         return self.title
--- src/products/views.py
+++ src/products/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render,get_object_or_404
+from django.http import Http404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Product
 # Create your views here.
 
 class ProductListView(ListView):
-    queryset=Product.objects.all()
+    # queryset=Product.objects.all()
     template_name="products/list.html"
 
     # def get_context_data(self,*args,**kwargs):
     #     context=super(ProductListView,self).get_context_data(*args,**kwargs)
     #     print(context)
     #     return context
+
+    def get_queryset(self,*args,**kwargs):
+        request=self.request
+        return Product.objects.all()
 
 def product_list_view(request):
     queryset=Product.objects.all()
@@ -21,18 +26,37 @@ def product_list_view(request):
     return render(request,"products/list.html",context) 
 
 class ProductDetailView(DetailView):
-    queryset=Product.objects.all()
+    # queryset=Product.objects.all()
     template_name="products/detail.html"
 
     def get_context_data(self,*args,**kwargs):
         context=super(ProductDetailView,self).get_context_data(*args,**kwargs)
         print(context)
         return context
+    
+    def get_object(self,*args,**kwargs):
+        request=self.request
+        pk=self.kwargs.get('pk')
+        instance=Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("product doesnt exisy")
+        raise instance
+    # def get_queryset(self,*args,**kwargs):
+    #     request=self.request
+    #     pk=self.kwargs.get('pk')
+    #     return Product.objects.filter(pk=pk)
 
 def product_detail_view(request,pk=None,*args,**kwargs):
-    instance=Product.objects.get(pk=pk)
-    instance=get_object_or_404(Product,pk=pk)
-    queryset=Product.objects.all()
+    # instance=Product.objects.get(pk=pk)
+    # instance=get_object_or_404(Product,pk=pk)
+    instance=Product.objects.get_by_id(pk)
+    if instance is None:
+        raise Http404("Product doesnt exists")
+    # qs=Product.objects.filter(id=pk)
+    # if qs.exists() and qs.count() ==1:
+    #     instance=qs.first()
+    # else:
+    #     raise Http404("Product doesnt exists")
     context={
         'object': instance
     }
