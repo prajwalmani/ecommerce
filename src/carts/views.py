b42d97eec269f56ca459785484a6ba0e803c5ae0
Prajwal Mani <prajwalmani@users.noreply.github.com>
2020-12-08 01:12:05 +0530

    cart  session adds

--- src/carts/views.py
+++ src/carts/views.py
@@ -3,18 +3,17 @@ from .models import Cart
 # Create your views here.
 
 def cart_create(user=None):
-    cart_obj=Cart.objects.create(user=None)
+    cart_obj,new_obj=Cart.objects.create(user=None)
     return cart_obj
 
 def cart_home(request):
-    request.session['cart_id'] ='12'
-    cart_id=request.session.get("cart_id",None)
-    qs=Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        cart_obj== qs.first()
-    else:
-        cart_obj=Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
+    cart_obj,new_obj=Cart.objects.new_or_get(request  )
+    products=cart_obj.products.all()
+    total=0
+    for x in products:
+        total +=x.price 
+    cart_obj.total=total
+    cart_obj.save()
     return render(request,"carts/home.html",{})    
 
 
